b6503d5efa9b5905d080f2ddc6bb3d7f1d51fd0b
Sunflower339 <nik.bazhuhin@gmail.com>
2025-06-28 15:30:22 -0400

    made icon for guided rockets and to work

--- Scenes/weapon_cooldown_container.gd
+++ Scenes/weapon_cooldown_container.gd
@@ -1,13 +1,20 @@
-extends PanelContainer
+extends HBoxContainer
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
 	$PrimaryCooldownBar.value = $PrimaryCooldownTimer.time_left
+	$SecondaryCooldownBar.value = $SecondaryCooldownTimer.time_left
 
 
 func _on_space_plane_shoot_primary(_pos: Vector2, cooldown_time: float) -> void:
 	$PrimaryCooldownBar.max_value = cooldown_time
 	$PrimaryCooldownTimer.wait_time = cooldown_time
 	$PrimaryCooldownTimer.start()
+
+
+func _on_space_plane_shoot_secondary(pos: Vector2, cooldown_time: float) -> void:
+	$SecondaryCooldownBar.max_value = cooldown_time
+	$SecondaryCooldownTimer.wait_time = cooldown_time
+	$SecondaryCooldownTimer.start()
