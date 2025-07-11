2738bca33d9bfbbd2c2879085ae220863eebb8a8
Sunflower339 <nik.bazhuhin@gmail.com>
2025-07-11 15:37:30 -0400

    added a way to toggle the auto fire of weapons off and on

--- Scripts/Plane/space_plane.gd
+++ Scripts/Plane/space_plane.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var mouse_pos:Vector2
 var direction:Vector2
+var primary_ready = true
+var secondary_ready = true
 var speed = 0
 var distance = 0
 const max_speed = 5000
@@ -16,6 +18,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	#NOTE: Input code for toggling auto fire mode
+	if (Input.is_action_just_pressed("toggle_primary_auto_fire")):
+		if ($PrimaryTimer.one_shot == true): #button pressed when auto fire is off
+			$PrimaryTimer.one_shot = false
+			$PrimaryTimer.start()
+			if (primary_ready == true):
+				shoot_primary.emit(global_position, $PrimaryTimer.wait_time)
+				primary_ready = false
+		else:
+			$PrimaryTimer.one_shot = true
+	if (Input.is_action_just_pressed("toggle_secondary_auto_fire")):
+		if ($SecondaryTimer.one_shot == true): #button pressed when auto fire is off
+			$SecondaryTimer.one_shot = false
+			$SecondaryTimer.start()
+			if (secondary_ready == true):
+				shoot_secondary.emit(global_position, $SecondaryTimer.wait_time)
+				secondary_ready = false
+		else:
+			$SecondaryTimer.one_shot = true
+	
 	mouse_pos = get_global_mouse_position()
 	direction = (mouse_pos - position).normalized()
 	distance = position.distance_to(mouse_pos)
@@ -27,14 +49,16 @@ func _process(_delta):
 		velocity = direction*speed
 	move_and_slide()
 	
-	
-	#$CanvasLayer/Label.text = "speed: " + str(speed)
-
-
 
 func _on_primary_timer_timeout():
-	shoot_primary.emit(global_position, $PrimaryTimer.wait_time)
+	if ($PrimaryTimer.one_shot == false):
+		shoot_primary.emit(global_position, $PrimaryTimer.wait_time)
+	else:
+		primary_ready = true
 
 
 func _on_secondary_timer_timeout():
-	shoot_secondary.emit(global_position, $SecondaryTimer.wait_time)
+	if ($SecondaryTimer.one_shot == false):
+		shoot_secondary.emit(global_position, $SecondaryTimer.wait_time)
+	else:
+		secondary_ready = true
